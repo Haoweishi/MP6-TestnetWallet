668df153ed0fd499b74ef5d7206a48240c8d8830
Haowei Shi <haoweis2@illinois.edu>
2018-12-03 17:21:08 -0600

    Finished key validity checker, displays message to user on invalid keys.

--- app/src/main/java/edu/illinois/cs/cs125/simplebtctestwallet/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/simplebtctestwallet/MainActivity.java
@@ -2,9 +2,16 @@ package edu.illinois.cs.cs125.simplebtctestwallet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.bitcoinj.core.AddressFormatException;
+import org.bitcoinj.core.Base58;
+
+import java.math.BigInteger;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
     public void goToNewWallet(View view) {
         Intent goToNewPage = new Intent(this, NewWallet.class);
         startActivity(goToNewPage);
+    }
+
+    public void useExsistingWallet(View view) {
+        EditText savedkeyinput = findViewById(R.id.entryform);
+        String str = savedkeyinput.getText().toString();
+        try {
+            str = wifToPrivKey(str);
+            Toast.makeText(getBaseContext(), "Decoded key: " + str, Toast.LENGTH_LONG).show();
+        } catch (AddressFormatException error) {
+            Log.e("Invalid addr", error.toString());
+            Toast.makeText(getBaseContext(), "Invalid wallet import format!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private static String wifToPrivKey(String inputWIF) throws AddressFormatException {
+        BigInteger dec = Base58.decodeToBigInteger(inputWIF);
+        String hexstr = dec.toString(16);
+        hexstr = hexstr.substring(0, hexstr.length() - 8);
+        hexstr = hexstr.substring(2, hexstr.length());
+        return hexstr.toUpperCase();
     }
 }
